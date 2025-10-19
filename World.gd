84ba5ac505ce4e3b3ec156a10c55155b30d670a2
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2025-10-19 23:19:42 +0200

    add projects

---
+++ World.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var borders = Rect2(1,1,38,21)
+
+onready var tileMap = $TileMap
+
+
+func _ready():
+	randomize()
+	generate_level()
+	
+	
+func generate_level():
+	var digger = Digger.new(Vector2(19,11), borders)
+	var map = digger.digs(500)
+	digger.queue_free()
+	
+	for location in map:
+		tileMap.set_cellv(location, -1)
+	
+	tileMap.update_bitmask_region(borders.position, borders.end)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
